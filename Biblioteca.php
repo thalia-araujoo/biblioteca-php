1dff9ba9d653c77574d841d287c7fc8a442e8aae
Liah Souza <115299056+thalia-araujoo@users.noreply.github.com>
2024-07-29 18:24:30 -0300

    Update Biblioteca.php

---
+++ Biblioteca.php
@@ -0,0 +1,27 @@
+<?php
+class Biblioteca {
+    private $livros = [];
+    private $usuarios = [];
+
+    public function adicionarLivro(Livro $livro) {
+        //faz a verificação
+    }
+
+    public function removerLivro(Livro $livro) {
+        //faz a verificação
+    }
+
+    public function adicionarUsuario(Usuario $usuario) {
+        //faz a verificação
+    }
+
+    public function emprestarLivro(Livro $livro, Usuario $usuario) {
+        //faz a verificação
+    }
+
+    public function devolverLivro(Livro $livro, Usuario $usuario) {
+        //faz a verificação
+    }
+
+}
+?>
